f427d9aed6b5ceb9ddc8b293dc1e22f8c9823d61
ZlayaKisya <116946888+zloykotept@users.noreply.github.com>
2025-03-02 21:13:40 +0200

    Update arch_post_instalation.sh

--- arch_post_instalation.sh
+++ arch_post_instalation.sh
@@ -1,7 +1,7 @@
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # install stuff
-sudo pacman -S libreoffice-fresh alsa-utils keepassxc lxappearance feh imagemagick lapce feh firefox
+sudo pacman -S libreoffice-fresh alsa-utils keepassxc lxappearance feh imagemagick lapce firefox waybar
 echo "run 
 git clone https://aur.archlinux.org/yay-git.git
 sudo mv yay-git /opt/
