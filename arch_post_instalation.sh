d2604688e703b07d7c88b868b00102188e2790fc
ZlayaKisya <116946888+zloykotept@users.noreply.github.com>
2025-03-02 21:20:17 +0200

    Update arch_post_instalation.sh

--- arch_post_instalation.sh
+++ arch_post_instalation.sh
@@ -8,4 +8,5 @@ sudo mv yay-git /opt/
 cd /opt/yay-git
 makepkg -si
 yay -S ulauncher
-terminal color text - #eadbab background #282828"
+terminal color text - #eadbab background #282828
+rice repo: https://github.com/levelednain/nainism-desktop/tree/sway"
