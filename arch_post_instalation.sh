db1e3128d271becd99d52e257aa1d0577206aa79
ZlayaKisya <116946888+zloykotept@users.noreply.github.com>
2025-03-02 18:29:41 +0200

    Create arch_post_instalation.sh

---
+++ arch_post_instalation.sh
@@ -0,0 +1,4 @@
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# install stuff
+sudo pacman -S libreoffice-fresh alsa-utils keepassxc lxappearance feh imagemagick lapce
