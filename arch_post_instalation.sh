c6da175c1e5dd8620f8749c90dd2900be001b5fe
ZlayaKisya <116946888+zloykotept@users.noreply.github.com>
2025-03-03 08:59:11 +0200

    Update arch_post_instalation.sh

--- arch_post_instalation.sh
+++ arch_post_instalation.sh
@@ -2,6 +2,9 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # install stuff
 sudo pacman -S libreoffice-fresh alsa-utils keepassxc lxappearance feh imagemagick lapce firefox waybar nemo nano gnome-terminal fzf
+mkdir -p ~/.local/share/fonts
+cp AgaveNerdFont-Regular.ttf ~/.local/share/fonts/
+cp JetBrainsMono-Regular.ttf ~/.local/share/fonts/
 echo "run 
 git clone https://aur.archlinux.org/yay-git.git
 sudo mv yay-git /opt/
